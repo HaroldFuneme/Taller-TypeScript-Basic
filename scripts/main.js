d52da2fc9bee87f2e679562fc8c7aaccd698f06a
harold funeme <h.funeme@uniandes.edu.co>
2022-08-18 23:20:07 +0000

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz";
+export var ap = new Aprendiz("Harold Hernando", "Funeme Molano", "avatar.png", 33, NivelEducativo.POSTGRADO);
+console.log(ap);
