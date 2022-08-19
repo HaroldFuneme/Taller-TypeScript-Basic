d0aa4a596c430ab249c66d6022a2c198c3aeb06a
harold funeme <h.funeme@uniandes.edu.co>
2022-08-19 00:35:33 +0000

    fix path of aprendiz

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Aprendiz, NivelEducativo } from "./aprendiz";
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 
 export const ap = new Aprendiz("Harold Hernando", "Funeme Molano", "avatar.png", 33, NivelEducativo.POSTGRADO);
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
-import { Aprendiz, NivelEducativo } from "./aprendiz";
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 export var ap = new Aprendiz("Harold Hernando", "Funeme Molano", "avatar.png", 33, NivelEducativo.POSTGRADO);
 console.log(ap);
