0e122514965200430dbb568249ebb6e5a16eaac8
harold funeme <h.funeme@uniandes.edu.co>
2022-08-19 00:50:17 +0000

    change DOM of HTML dynamically

--- main.ts
+++ main.ts
@@ -3,3 +3,21 @@ import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 export const ap = new Aprendiz("Harold Hernando", "Funeme Molano", "avatar.png", 33, NivelEducativo.POSTGRADO);
 
 console.log(ap);
+console.log(ap);
+
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
+    let tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}" height="100"></td></tr>
+    <tr><td>Nombres: </td><td>${aprendiz.nombre}</td></tr>
+    <tr><td>Apellidos: </td><td>${aprendiz.apellido}</td></tr>
+    <tr><td>Nivel Educativo: </td><td>${aprendiz.nivelEducativo}</td></tr>
+    <tr><td>Edad: </td><td>${aprendiz.edad}</td></tr>
+    `
+    aprendizTable.appendChild(tbodyAprendiz);
+
+}
+
+mostrarDatosAprendiz(ap);
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 export var ap = new Aprendiz("Harold Hernando", "Funeme Molano", "avatar.png", 33, NivelEducativo.POSTGRADO);
 console.log(ap);
+console.log(ap);
+var aprendizTable = document.getElementById("aprendiz");
+function mostrarDatosAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./" + aprendiz.avatar + "\" height=\"100\"></td></tr>\n    <tr><td>Nombres: </td><td>" + aprendiz.nombre + "</td></tr>\n    <tr><td>Apellidos: </td><td>" + aprendiz.apellido + "</td></tr>\n    <tr><td>Nivel Educativo: </td><td>" + aprendiz.nivelEducativo + "</td></tr>\n    <tr><td>Edad: </td><td>" + aprendiz.edad + "</td></tr>\n    ";
+    aprendizTable.appendChild(tbodyAprendiz);
+}
+mostrarDatosAprendiz(ap);
